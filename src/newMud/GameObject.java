83077eee6bcecafe59034e6d2e9784d44da1faed
Justin Clitheroe <justinclitheroe@gmail.com>
2015-10-31 16:21:36 -0400

    Woot

--- src/newMud/GameObject.java
+++ src/newMud/GameObject.java
@@ -4,46 +4,81 @@ import java.util.ArrayList;
 
 
 public class GameObject {
-	private ArrayList<Item> inventory = new ArrayList<Item>(0);
-	private int xCoord;
-	private int yCoord;
-	private int id;
+	private ArrayList<Item> inventory = new ArrayList<Item>(); //We'll see if I can get away with this. Easy fix if I can't
+	private Room location;
+	private String name;
+	private String description;
 	
 	//Methods
 	
 	public void pickUp(Item i) {
-		inventory.add(i);
+		if (this.getLocation().getInventory().contains(i))
+			inventory.add(i);
 	}
 	
 	public void drop(Item i){
-		int indexOf = inventory.indexOf(i);
-		inventory.remove(indexOf);
+		inventory.remove(i);
+		this.getLocation().getInventory().add(i);
+	}
+	
+	public Boolean checkItem(String item_name){              //Checks to see if an item is in the array of Items in current room
+		for(int i = 0; i < this.getInventory().size() ; i++){
+			if (this.getInventory().get(i).getName().equalsIgnoreCase(item_name)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Item returnItem(String item_name){     			//returns an item based on the string item_name
+		for(int i = 0; i < this.getInventory().size() ; i++){
+			if (this.getInventory().get(i).getName().equalsIgnoreCase(item_name)){
+				Item tempItem =this.getInventory().get(i);
+				this.getInventory().remove(i);
+				return tempItem;
+			}
+		}
+		return null;
 	}
 	
 	//Getters n' Setters
 	
-	public int getyCoord() {
-		return yCoord;
-	}
 	
-	public void setyCoord(int yCoord) {
-		this.yCoord = yCoord;
-	}
-	
-	public int getxCoord() {
-		return xCoord;
-	}
-	
-	public void setxCoord(int xCoord) {
-		this.xCoord = xCoord;
+	public Room getLocation() {
+		return location;
 	}
 
-	public int getId() {
-		return id;
+	public void setLocation(Room location) {
+		this.location = location;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public ArrayList<Item> getInventory() {
+		return inventory;
+	}
+
+
+	public void setInventory(ArrayList<Item> inventory) {
+		this.inventory = inventory;
+	}
+	
+	public String toString() {
+		return name + ", " + description;
 	}
 	
 }
--- src/newMud/Item.java
+++ src/newMud/Item.java
@@ -2,4 +2,10 @@ package newMud;
 
 public class Item extends GameObject {
 	
+	public Item(String n, String d) {
+		this.setName(n);
+		this.setDescription(d);
+	}
+	
+	
 }
