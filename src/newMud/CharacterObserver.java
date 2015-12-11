a7dbd773655fed6eb41abc14bc65adc9a22513bf
Justin Clitheroe <justinclitheroe@gmail.com>
2015-12-11 11:23:15 -0500

    message

---
+++ src/newMud/CharacterObserver.java
@@ -0,0 +1,14 @@
+package newMud;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class CharacterObserver implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
